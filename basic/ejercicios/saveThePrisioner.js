7e2b82f5659de0e1f2ac6fa4749cf8dd43584f32
jsanchezperuga <jeronimo.sanchez@ort.edu.ar>
2021-09-08 00:22:44 -0300

    Jeronimo Sanchez Peruga. Agrego mi solución del ejercicio saveThePrisioner.js

--- basic/ejercicios/saveThePrisioner.js
+++ basic/ejercicios/saveThePrisioner.js
@@ -14,9 +14,29 @@
  * @param {Number} s el numero de silla por donde se empieza
  * @return el numero de prisionero que recibe la ultima golosina
  */
-const saveThePrisoner = function(n,m,s){ 
-     
+
+//primer intento malísimo, ni me puse a mirar qué es lo que está mal
+// const saveThePrisoner = function(n,m,s){
+//       console.log(`Prisioneros: ${n} Caramelos: ${m} Silla: ${s}`);
+//       while(m>1){
+//          s++;
+//          if(s===n){
+//             s=1;
+//          }
+//          m--;
+//       }
+//       return s;
+// }
+
+/*segundo intento luego de mucho dibujito en papel
+. Comencé aplicando el módulo de la cantidad de caramelos y la cantidad de presos, de esa manera me ahorro las iteraciones y el módulo representa la cantidad de caramelos de la última ronda
+. después de mucho ensayo, encontré la relación entre el módulo, la silla de inicio y la silla del último caramelo: silla de inicio + módulo -1 es igual a la última silla.
+. entonces le sumo a la silla de inicio el módulo (le resto uno porque el caramelo de esa silla ya fue contado) y si el número resultante es mayor a la cantidad de prisioneros le resto esa cantidad para que empiece a contar desde el principio y el resultado es el nro de silla del último caramelo.
+*/
+const saveThePrisoner = function(n,m,s){
+ return (s += m%n -1) > n ? s-=n : s; 
 }
+
 
 // TEST TDD
 console.log(saveThePrisoner(5,2,1) === 2);
